730fd5ac9213627876814efc75c2b0b5a07fd8dc
Mofifoluwa Leke-Akinrowo <n01343651@humbermail.ca>
2021-09-26 01:43:39 -0400

    Commit 4, full of errors yet to be corrected

--- SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/MainActivity.java
+++ SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/MainActivity.java
@@ -1,76 +1,114 @@
 package ca.greenops.it.smartbuilding;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.View;
+import com.quintus.labs.smarthome.R;
+import ca.greenops.it.smartbuilding.RoomAdapter;
+import ca.greenops.it.smartbuilding.Room;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import java.util.ArrayList;
+import java.util.List;
 
-import ca.greenops.it.smartbuilding.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
+/**
+ * Smart Home
+ * https://github.com/quintuslabs/SmartHome
+ * Created on 27-OCT-2019.
+ * Created by : Santosh Kumar Dash:- http://santoshdash.epizy.com
+ */
 public class MainActivity extends AppCompatActivity {
+    private List<Room> roomList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RoomAdapter mAdapter;
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    RelativeLayout home_rl, time_rl, setting_rl, scene_rl;
+
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        //make fully Android Transparent Status bar
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        home_rl = findViewById(R.id.home_rl);
+        time_rl = findViewById(R.id.time_rl);
+        scene_rl = findViewById(R.id.scene_rl);
+        setting_rl = findViewById(R.id.setting_rl);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        recyclerView = findViewById(R.id.recycler_view);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        home_rl.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                home_rl.setBackgroundResource(0);
             }
         });
+
+        mAdapter = new RoomAdapter(roomList, getApplicationContext());
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        prepareRoomData();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void prepareRoomData() {
+        Room room = new Room("1", "BedRoom");
+        roomList.add(room);
+        room = new Room("2", "Kitchen");
+        roomList.add(room);
+        room = new Room("1", "Bathroom");
+        roomList.add(room);
+        room = new Room("2", "Hall");
+        roomList.add(room);
+        room = new Room("1", "Dining");
+        roomList.add(room);
+        room = new Room("2", "Kitchen");
+        roomList.add(room);
+        room = new Room("1", "BedRoom");
+        roomList.add(room);
+        room = new Room("2", "Kitchen");
+        roomList.add(room);
+        room = new Room("1", "BedRoom");
+        roomList.add(room);
+        room = new Room("2", "Kitchen");
+        roomList.add(room);
+        room = new Room("1", "BedRoom");
+        roomList.add(room);
+        room = new Room("2", "Kitchen");
+        roomList.add(room);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        mAdapter.notifyDataSetChanged();
     }
 }
