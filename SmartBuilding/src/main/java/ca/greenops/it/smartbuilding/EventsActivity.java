624c989aff52f15b82e0d1a5b56204cfd4cfa192
Andrew <n01309442@humbermail.ca>
2021-09-27 17:48:25 -0400

    -check the state of the stats and display them in events textView

--- SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/EventsActivity.java
+++ SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/EventsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,7 @@ public class EventsActivity extends AppCompatActivity {
     private String roomTemp, humidity;
     private String fireStat;
     private String doorStat;
+    private TextView eventView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,37 @@ public class EventsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        eventView = (TextView) findViewById(R.id.eventView);
+
+        eventView.setText(printEvents());
+
+    }
+
+    public String printEvents()
+    {
+        String line1, line2,line3;
+         line1 = getString(R.string.temp)+roomTemp+"\t"+getString(R.string.humid)+humidity;
+
+        if (doorStat.equals(R.string.open))
+        {
+            line2 = getString(R.string.doorOpen);
+        }
+        else
+        {
+            line2 = getString(R.string.doorClose);
+        }
+
+        if (fireStat.equals(getString(R.string.on)))
+        {
+            line3 = getString(R.string.fireAlarmOn);
+        }
+        else
+        {
+            line3 = getString(R.string.fireAlarmOff);
+        }
+
+                return line1+"\n"+line2+"\n"+line3;
 
     }
 
