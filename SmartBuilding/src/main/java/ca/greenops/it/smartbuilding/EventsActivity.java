be64960598e41a9053e8c031bcb033b2df89349b
Andrew <n01309442@humbermail.ca>
2021-09-27 15:58:33 -0400

    - create layout and java file for events

---
+++ SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/EventsActivity.java
@@ -0,0 +1,27 @@
+package ca.greenops.it.smartbuilding;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+
+
+public class EventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.events);
+
+        ActionBar actionBar = this.getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
