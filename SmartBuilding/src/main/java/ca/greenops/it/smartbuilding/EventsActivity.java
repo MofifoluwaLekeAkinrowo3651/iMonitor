72578eb5e0e4f9d0cef8258d28206419cdefd8a2
Mofifoluwa Leke-Akinrowo <n01343651@humbermail.ca>
2021-10-11 19:24:35 -0400

    Deliverable 2 - Final commit

--- SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/EventsActivity.java
+++ SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/EventsActivity.java
@@ -17,7 +17,7 @@ public class EventsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.events);
+        setContentView(R.layout.activity_events);
 
         ActionBar actionBar = this.getSupportActionBar();
 
