d6d259131b0b6abd61f09846bfad6f223c9cea64
Andrew <n01309442@humbermail.ca>
2021-11-13 11:21:28 -0500

    -add functionality to submit btn -When the submit btn is pressed, the user's rating is displayed in a textView

--- SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/ReviewActivity.java
+++ SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/ReviewActivity.java
@@ -1,6 +1,10 @@
 package ca.greenops.it.smartbuilding;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,19 @@ public class ReviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+
+        final RatingBar ratingbar = (RatingBar) findViewById(R.id.ratingBar);
+        Button button = (Button) findViewById(R.id.submit_btn);
+        final TextView textView = (TextView) findViewById(R.id.ratingView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                 textView.setText(getString(R.string.yourRating) + ratingbar.getRating());
+
+            }
+        });
     }
 }
