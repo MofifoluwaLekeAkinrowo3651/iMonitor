b5e6a5652753cb934d23538902194d4fe89e7f97
Mofifoluwa Leke-Akinrowo <n01343651@humbermail.ca>
2021-11-28 21:58:37 -0500

    Created RegistrationActivity.java and resource file. Added to Manifest.

---
+++ SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/RegistrationActivity.java
@@ -0,0 +1,15 @@
+package ca.greenops.it.smartbuilding;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
