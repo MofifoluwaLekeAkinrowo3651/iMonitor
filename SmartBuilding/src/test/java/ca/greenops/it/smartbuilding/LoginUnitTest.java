c03d4969eeb905f38d96ff6f6fe21312e4b42fee
Mofifoluwa Leke-Akinrowo <n01343651@humbermail.ca>
2021-12-03 01:33:04 -0500

    JUnit Test Cases using Mockito

---
+++ SmartBuilding/src/test/java/ca/greenops/it/smartbuilding/LoginUnitTest.java
@@ -0,0 +1,34 @@
+package ca.greenops.it.smartbuilding;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.android.gms.common.internal.IAccountAccessor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LoginUnitTest {
+
+    @Test
+    public void bademail_goodpass () {
+
+    }
+
+    @Test
+    public void goodemail_badpass () {
+
+    }
+
+    @Test
+    public void bademail_badpass () {
+
+    }
+
+    @Test
+    public void goodemail_goodpass () {
+
+    }
+}
