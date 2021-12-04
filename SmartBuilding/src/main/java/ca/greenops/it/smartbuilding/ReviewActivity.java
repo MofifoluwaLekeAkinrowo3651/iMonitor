af3e0112fd0462cd1f76dfabd5bf676fddf3c715
Andrew <n01309442@humbermail.ca>
2021-12-04 12:01:19 -0500

    -update build.gradle to build Robolectric -create test ReviewActivityTest

--- SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/ReviewActivity.java
+++ SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/ReviewActivity.java
@@ -13,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.junit.runner.RunWith;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
---
+++ SmartBuilding/src/test/java/ReviewActivityTest.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+
+@RunWith(RobolectricTestRunner.class)
+public class ReviewActivityTest{
+
+}
