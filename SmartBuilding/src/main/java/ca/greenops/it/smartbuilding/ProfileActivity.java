c262e27dcf93bb1f52df05c55b8d5f220e09f989
bd094 <bd0942630@gmail.com>
2021-12-04 22:37:24 -0500

    Getting phone number in profile screen

--- SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/ProfileActivity.java
+++ SmartBuilding/src/main/java/ca/greenops/it/smartbuilding/ProfileActivity.java
@@ -11,20 +11,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
-    TextView textView1,textView2;
-    String name,email;
+    TextView textView1,textView2,textView3;
+    String name,email,phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         textView1 = (TextView) findViewById(R.id.profileUserName);
         textView2 = (TextView) findViewById(R.id.profileEmail);
+        textView3 = (TextView) findViewById(R.id.profilePhone);
 
         name=MainActivity.getActivityInstance().getData();
         textView1.setText(new StringBuilder().append(getString(R.string.user1)).append(name));
 
         email=LoginActivity.getActivityInstance2().getEmail();
         textView2.setText(new StringBuilder().append(getString(R.string.email1)).append(email));
+
+       // phone=ReviewActivity.getActivityInstance3().getPhone();
+        textView3.setText(new StringBuilder().append(getString(R.string.phone1)).append(phone));
     }
 
     @Override
