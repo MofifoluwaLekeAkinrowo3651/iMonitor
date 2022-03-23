2fa7f2addd6f00ae2a6efc0bf179e5254ad9700b
Mofifoluwa Dieko Akinrowo <77804739+MofifoluwaLekeAkinrowo3651@users.noreply.github.com>
2022-03-23 10:28:52 -0400

    Update HCSR04.py
    Continuous distance update

--- HCSR04.py
+++ HCSR04.py
@@ -21,41 +21,37 @@ GPIO.setup(ECHO1, GPIO.IN)
 GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output and set initial value
 GPIO.output(TRIG1, GPIO.LOW)
 
+def dist():
+
+    GPIO.output(TRIG1, True)
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(TRIG1, False)
+
+    while GPIO.input(ECHO1) == 0:
+        pulse_start1 = time.time()
+ 
+    while GPIO.input(ECHO1) == 1:
+        pulse_end1 = time.time()
+
+    pulse_duration1 = pulse_end1 - pulse_start1
+
+    distance1 = pulse_duration1 * 17150
+    distance1 = round(distance1, 2)
+    return distance1
+
 print ("Waiting For HC-SR04 Sensor To Settle")
 time.sleep(2)
 print ("Calculating Distance")
 GPIO.output(TRIG1, GPIO.HIGH)
-time.sleep(.00001)
+time.sleep(1)
 GPIO.output(TRIG1, GPIO.LOW)
+            
+while True:
+    result = dist()
+    print("Distance: ", result, "cm")
+    time.sleep(1)
+    db.set_distance("distance", result)
 
-while GPIO.input(ECHO1) == 0:
-    pulse_start1 = time.time()
- 
-while GPIO.input(ECHO1) == 1:
-    pulse_end1 = time.time()
-
-pulse_duration1 = pulse_end1 - pulse_start1
-
-distance1 = pulse_duration1 * 17150
-distance1 = round(distance1, 2)
-db.set_distance("distance", distance1)
-
-if distance1 > 0.00 and distance1 < 15:
-        print("Distance: ",distance1, "cm")
-        GPIO.output(LED, GPIO.HIGH) # Turn on
-        time.sleep(2) 
-        GPIO.output(LED, GPIO.LOW) # Turn off
-        time.sleep(1)
-        GPIO.output(LED, GPIO.HIGH) # Turn on
-        time.sleep(2)
-        GPIO.output(LED, GPIO.LOW) # Turn off
-        time.sleep(1)
-        GPIO.output(LED, GPIO.HIGH) # Turn on
-        time.sleep(3)
-        
-else:
-        #print "Distance: ",distance1, "cm"
-        GPIO.output(LED, GPIO.HIGH) # Turn on
-        time.sleep(25)
 
 GPIO.cleanup()
